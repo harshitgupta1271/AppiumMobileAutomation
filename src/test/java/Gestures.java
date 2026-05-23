0f0958db1cb6d170d219cd6fb96ee50f09da558e
harshit Gupta <hgupta1271@gmail.com>
2026-05-23 10:03:45 +0530

    2

---
+++ src/test/java/Gestures.java
@@ -0,0 +1,35 @@
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Gestures extends BaseTest {
+    @Test
+    public void gestureTest() throws InterruptedException {
+
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+
+        for (int i = 1; i <= 5; i++) {
+
+            WebElement ele = driver.findElement(
+                    By.xpath("(//android.widget.TextView)[" + i + "]")
+            );
+
+            System.out.println(ele.getText());
+        }
+        WebElement ele = driver.findElement(By.xpath("(//android.widget.TextView)[2]"));
+        Assert.assertNotNull(((RemoteWebElement) ele).getId());
+        ((JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) ele).getId(),
+                "duration",2000
+        ));
+        Thread.sleep(3000);
+    }
+
+}
